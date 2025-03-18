bb204bc9429902884b46fb9de6c089d7c65a58bf
Barun Ghorai <ranghorai865@gmail.com>
2025-03-19 00:18:45 +0530

    From here we can run our program

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python-path
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ =='__main__':
     main()
